3c6079c4a13bbc5bb68c5cbe3b634564c5e6c65c
xl <306619887@qq.com>
2019-05-30 10:42:27 +0800

    modify podspec

---
+++ Example/Pods/Target Support Files/Pods-XLLKit_Example/Pods-XLLKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XLLKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XLLKit_Example
+@end
---
+++ Example/Pods/Target Support Files/XLLKit/XLLKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XLLKit : NSObject
+@end
+@implementation PodsDummy_XLLKit
+@end
