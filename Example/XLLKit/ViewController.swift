2738e1652f41de64f0f8be8902740144753b7bbd
xl <306619887@qq.com>
2019-05-31 17:37:32 +0800

    commit extension

--- Example/XLLKit/ViewController.swift
+++ Example/XLLKit/ViewController.swift
@@ -14,14 +14,28 @@ class ViewController: UIViewController , UITableViewDelegate{
     override func viewDidLoad()
     {
         super.viewDidLoad()
-//        let a = UIView.xl_createView(backgroundColor: .yellow)
-//        a.frame = CGRect(x: 50, y: 200, width: 200, height: 200)
-//        self.view.addSubview(a)
-//        self.view.xl_setViewCornerRadius(roundingCorners: [.allCorners], cornerRadius: 100, color: .red)
+
+        
+    }
+    
+    func test()
+    {
+        print(xl_appName)
+        print(xl_appBuild)
+        print(xl_appVersion)
+        print(xl_appBundleID)
+        self.view.backgroundColor = UIColor.xl_hexString("0x112340")
+        let a = UIImage().xl_circleImage()
+        print(a)
+        
+        //        let a = UIView.xl_createView(backgroundColor: .yellow)
+        //        a.frame = CGRect(x: 50, y: 200, width: 200, height: 200)
+        //        self.view.addSubview(a)
+        //        self.view.xl_setViewCornerRadius(roundingCorners: [.allCorners], cornerRadius: 100, color: .red)
         let text = "daljldkjfdlkajskldfjdklafjfajfdkljdlff!!!"
         let height = (text as NSString).boundingRect(with: CGSize(width: 200, height: 9999), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 30)], context: nil).size.height
         let tv = UITextView(frame: CGRect(x: 20, y: 88, width: 200, height: height))
-        let size = tv.contentSize
+        // let size = tv.contentSize
         tv.text = text
         tv.isSelectable = false
         tv.isEditable = false
@@ -40,18 +54,6 @@ class ViewController: UIViewController , UITableViewDelegate{
         label.layer.borderColor = UIColor.red.cgColor
         label.layer.borderWidth = 1.0;
         label.numberOfLines = 0
-        
-    }
-    
-    func test()
-    {
-        print(xl_appName)
-        print(xl_appBuild)
-        print(xl_appVersion)
-        print(xl_appBundleID)
-        self.view.backgroundColor = UIColor.xl_hexString("0x112340")
-        let a = UIImage().xl_circleImage()
-        print(a)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
