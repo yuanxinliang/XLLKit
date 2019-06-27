df2074e74fc8acafe2dd632327cf12816bc00639
xl <306619887@qq.com>
2019-06-27 16:57:26 +0800

    commit

--- XLLKit/Classes/Extension/String+Extension.swift
+++ XLLKit/Classes/Extension/String+Extension.swift
@@ -284,5 +284,17 @@ public extension String
             }
         }
     }
+    
+    /// 根据开始位置和长度截取字符串
+    func subString(start: Int, length: Int = -1) -> String
+    {
+        var len = length
+        if len == -1 {
+            len = self.count - start
+        }
+        let st = self.index(startIndex, offsetBy: start)
+        let en = self.index(st, offsetBy: len)
+        return String(self[st ..< en])
+    }
 }
 
